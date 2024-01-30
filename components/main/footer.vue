75a6d1f75714bd7fd469a3deece7305d109f4a45
jongju <jongju0920@kakao.com>
2024-01-30 23:27:02 +0900

    폴스타인으로 이름 수정

--- components/main/footer.vue
+++ components/main/footer.vue
@@ -36,7 +36,7 @@
     <div class="pt-0 flex-1-1-100">
       <VDivider color="white mt-10 mb-3" :thickness="2" />
 
-      FRONTIER <span class="ml-1 mr-1">|</span> 대표전화 : <a href="tel:010-7607-4451">010-7607-4451</a> <span class="ml-1 mr-1">|</span> 팩스 :
+      POLSTARIN <span class="ml-1 mr-1">|</span> 대표전화 : <a href="tel:010-7607-4451">010-7607-4451</a> <span class="ml-1 mr-1">|</span> 팩스 :
       <a href="fax:0504-163-4451">0504-163-4451</a> <span class="ml-1 mr-1">|</span> 이메일 :
       <a href="mailto:jongju0920@kakao.com">jongju0920@kakao.com</a> <br />
       주소 : 경기 성남시 분당구 성남대로331번길 8 9층 901호
