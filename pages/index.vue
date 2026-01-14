3588031312d16733cf2a9ddda0690da1875aed40
jongjupark <jongju0920@kakao.com>
2026-01-14 16:29:25 +0900

    메인 소개 텍스트 수정

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,7 @@
     <VRow class="main-row">
       <MainHeader />
       <section class="banner-text text-white">
-        <p>We are IT experts</p>
-        <p class="logo"></p>
-        <p style="line-height: 1.6">
-          폴스타인는 숙련된 개발자의 개발, 제조경험과
-          <br />
-          노하우를 바탕으로 고객의 비지니스를 지원합니다.
-          <br /><br />
-          유능한 전문 업체를 만나 제품과 서비스를 완성하고, 고객의 이야기를 가치로 바꿔보세요.
-        </p>
+        <p class="main-message">아이디어만 가져오세요.<br />같이 고민하면 길이 보입니다.</p>
       </section>
     </VRow>
     <MainBusinessArea />
@@ -38,27 +30,16 @@
       position: absolute;
       top: min(380px, 30vw);
       left: min(95px, 5vw);
-      font-size: 20px;
       margin-right: 5vw;
+      max-width: 600px;
 
-      p {
-        margin: 5px 0;
-        line-height: 25px;
-        text-shadow: 2px 2px 4px gray;
-
-        // &.title {
-        //   font-weight: 900;
-        //   font-size: 45px;
-        //   margin: 20px 0;
-        //   text-shadow: none;
-        // }
-        &.logo {
-          width: 301px;
-          height: 47px;
-          background-image: url('/logo/logo-white.png');
-          background-repeat: no-repeat;
-          margin: 10px 0 25px;
-        }
+      .main-message {
+        font-size: min(3.5rem, 8vw);
+        font-weight: 700;
+        line-height: 1.4;
+        margin: 0;
+        text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+        letter-spacing: -0.02em;
       }
     }
   }
